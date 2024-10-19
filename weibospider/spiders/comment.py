c3da47160153d12eb37e2fa1ced8e087bddbcc30
Vivian Xie <94162385+Vivian-Xie@users.noreply.github.com>
2024-10-19 15:32:52 +0800

    Add files via upload

--- weibospider/spiders/comment.py
+++ weibospider/spiders/comment.py
@@ -1,50 +1,45 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-Author: nghuyong
-Mail: nghuyong@163.com
-Created Time: 2020/4/14
-"""
 import json
-from scrapy import Spider
-from scrapy.http import Request
+from scrapy import Spider, Request
 from spiders.common import parse_user_info, parse_time, url_to_mid
 
-
 class CommentSpider(Spider):
-    """
-    微博评论数据采集
-    """
     name = "comment"
+    custom_settings = {
+        'LOG_LEVEL': 'INFO',  # Setting log level to INFO to reduce clutter
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(CommentSpider, self).__init__(*args, **kwargs)
+        self.unique_nicknames = set()  # Use a set to store unique nicknames
 
     def start_requests(self):
-        """
-        爬虫入口
-        """
-        # 这里tweet_ids可替换成实际待采集的数据
-        tweet_ids = ['Mb15BDYR0']
+        tweet_ids = ['OAaPqcG7M']
         for tweet_id in tweet_ids:
             mid = url_to_mid(tweet_id)
-            url = f"https://weibo.com/ajax/statuses/buildComments?" \
-                  f"is_reload=1&id={mid}&is_show_bulletin=2&is_mix=0&count=20"
+            url = f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={mid}&is_show_bulletin=2&is_mix=0&count=20"
             yield Request(url, callback=self.parse, meta={'source_url': url})
 
     def parse(self, response, **kwargs):
-        """
-        网页解析
-        """
         data = json.loads(response.text)
         for comment_info in data['data']:
             item = self.parse_comment(comment_info)
-            yield item
-            # 解析二级评论
+            nick_name = item['comment_user']['nick_name']
+            self.unique_nicknames.add(nick_name)
             if 'more_info' in comment_info:
-                url = f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={comment_info['id']}" \
-                      f"&is_show_bulletin=2&is_mix=1&fetch_level=1&max_id=0&count=100"
+                url = f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={comment_info['id']}&is_show_bulletin=2&is_mix=1&fetch_level=1&max_id=0&count=100"
                 yield Request(url, callback=self.parse, priority=20)
         if data.get('max_id', 0) != 0 and 'fetch_level=1' not in response.url:
             url = response.meta['source_url'] + '&max_id=' + str(data['max_id'])
             yield Request(url, callback=self.parse, meta=response.meta)
+        else:
+            # Once all pages have been scraped, write unique nicknames to the file
+            self.write_nicknames_to_file()
+
+    def write_nicknames_to_file(self):
+        with open('unique_comments.txt', 'w', encoding='utf-8') as file:
+            sorted_nicknames = sorted(self.unique_nicknames)
+            for index, nick in enumerate(sorted_nicknames, start=1):
+                file.write(f"{index}. {nick}\n")  # Write each unique nickname with an index
 
     @staticmethod
     def parse_comment(data):
--- weibospider/spiders/repost.py
+++ weibospider/spiders/repost.py
@@ -1,43 +1,54 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-Author: nghuyong
-Mail: nghuyong@163.com
-Created Time: 2020/4/14
-"""
 import json
-from scrapy import Spider
-from scrapy.http import Request
+from scrapy import Spider, Request
 from spiders.common import parse_tweet_info, url_to_mid
 
-
 class RepostSpider(Spider):
-    """
-    微博转发数据采集
-    """
     name = "repost"
+    custom_settings = {
+        'LOG_LEVEL': 'INFO',  # Reduce the amount of logged info to declutter the output
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(RepostSpider, self).__init__(*args, **kwargs)
+        self.unique_nicks = set()  # Use a set to store unique nicknames
 
     def start_requests(self):
-        """
-        爬虫入口
-        """
-        # 这里tweet_ids可替换成实际待采集的数据
-        tweet_ids = ['Mb15BDYR0']
+        tweet_ids = ['Ouf8067R1']
         for tweet_id in tweet_ids:
             mid = url_to_mid(tweet_id)
             url = f"https://weibo.com/ajax/statuses/repostTimeline?id={mid}&page=1&moduleID=feed&count=10"
             yield Request(url, callback=self.parse, meta={'page_num': 1, 'mid': mid})
 
     def parse(self, response, **kwargs):
-        """
-        网页解析
-        """
         data = json.loads(response.text)
         for tweet in data['data']:
             item = parse_tweet_info(tweet)
+            # Extract nickname and add to the set for uniqueness
+            nick_name = item['user']['nick_name']
+            self.unique_nicks.add(nick_name)  # Adds only if not already present
             yield item
         if data['data']:
             mid, page_num = response.meta['mid'], response.meta['page_num']
             page_num += 1
             url = f"https://weibo.com/ajax/statuses/repostTimeline?id={mid}&page={page_num}&moduleID=feed&count=10"
             yield Request(url, callback=self.parse, meta={'page_num': page_num, 'mid': mid})
+        else:
+            # Once all pages have been scraped, write unique nicknames to the file
+            self.write_nicks_to_file()
+
+    def write_nicks_to_file(self):
+        with open('unique_nick_names.txt', 'w', encoding='utf-8') as file:
+            for index, nick in enumerate(sorted(self.unique_nicks), start=1):
+                file.write(f"{index}. {nick}\n")  # Write each unique nickname with an index
+
+    def url_to_mid(self, tweet_id):
+        # Implement based on the actual conversion logic needed
+        return tweet_id
+
+    def parse_tweet_info(self, tweet):
+        # Actual parsing logic based on the JSON structure of the tweet
+        return {
+            'user': {
+                'nick_name': 'example_nick'  # This should be replaced with the correct field name
+            }
+        }
